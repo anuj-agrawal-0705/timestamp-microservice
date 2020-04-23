e5935c581c83efc065a6914aab2ef7639fb154c3
Anuj Agrawal <anujagrawal751997@gmail.com>
2020-04-24 02:40:40 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/',(req,res)=>{
-    res.send('yo bitches') 
+    res.send('yo bitches')
 })
 
 app.get('/api/timestamp/:date_string', (req,res)=>{
     const date_string = new Date (String(req.params.date_string))
     const data = new Date(Number(req.params.date_string))
-        // if(date_string == 'Invalid Date' && data == 'Invalid Date'){
-        //     res.send({error:"Invaid Date"})
-        // }
-        if(data != 'Invalid Date'){
+        if(date_string == 'Invalid Date' && data == 'Invalid Date'){
+             res.send({error:"Invaid Date"})
+         }
+        else if(data != 'Invalid Date'){
+            const unix = data.getTime()
             const utc = data.toUTCString()
             console.log('1')
-            return res.send({data, utc})
+            return res.send({unix, utc})
         }
         else if(date_string != 'Invalid Dte'){
             const unix = date_string.getTime()
             const utc = date_string.toUTCString()
-            console.log(new Date("4-25-2020"))
+            console.log('2')
             return res.send({unix, utc})
         }
 
