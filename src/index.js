70bc03036a95507884e40b74c18f0aff114d9810
Anuj Agrawal <anujagrawal751997@gmail.com>
2020-04-24 00:59:30 +0530

    node_modules removed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/',(req,res)=>{
-    res.send('yo bitches')
+    res.send('yo bitches') 
 })
 
 app.get('/api/timestamp/:date_string', (req,res)=>{
